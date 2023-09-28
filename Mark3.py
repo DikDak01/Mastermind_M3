ead7091631ae2efe9b4d10b453f8054ed71ff6a4
Nik Nak <72702814+DikDak01@users.noreply.github.com>
2023-09-28 19:40:13 +0200

    Add files via upload

---
+++ Mark3.py
@@ -0,0 +1,20 @@
+#Mastermind
+#Mark 3 V0.1
+
+from colorama import *
+import sqlite3
+import datetime
+
+
+#Initialize colorama
+init(autoreset=True)
+
+
+
+class Information():
+    
+    print("Hello, welcome to Mastermind")
+    print("Rules: ")
+                       
+                
+ 
